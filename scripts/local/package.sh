d0e3e662101248ae71ea2674d1aa3e295ba96c71
borjagilperez <borjagilperez@outlook.com>
2020-12-14 12:44:04 +0100

    refactor: conda environment moved

--- scripts/local/package.sh
+++ scripts/local/package.sh
@@ -20,7 +20,7 @@ select opt in "${options[@]}"; do
         "Install twitter_env")
             eval "$($HOME/miniconda/bin/conda shell.bash hook)"
             conda activate base && conda info --envs
-            conda env create -f ./scripts/local/environment.yml
+            conda env create -f ./src/main/python/environment.yml
             conda activate twitter_env && conda info --envs
             break
             ;;
@@ -29,7 +29,7 @@ select opt in "${options[@]}"; do
             eval "$($HOME/miniconda/bin/conda shell.bash hook)"
             conda activate base && conda info --envs
             conda remove -y -n twitter_env --all
-            conda env create -f ./scripts/local/environment.yml
+            conda env create -f ./src/main/python/environment.yml
             break
             ;;
 
